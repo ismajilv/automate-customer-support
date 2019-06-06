24e4b101e35f1e3ea12071ae6bed5f1ecd2b5fb7
ismajilv <tural.ismayilov.ti@gmail.com>
2019-06-06 21:38:55 +0300

    utils updated

--- flask/api/utils.py
+++ flask/api/utils.py
@@ -1,17 +1,33 @@
+import time
+import random
+
 def increment_request_count(user_id):
     pass
 
 
 def run_classifier():
-    pass
+    time.sleep(1) #mocked by 1 second 
+    
+    return True
 
 
 def get_external_data():
-    pass
+    """
+        I understood it like if the external api return response it should do in microseconds if not then waiting. 
+        I did it like, there is 90 percent of chance to get response immediately and 10 percent chance of waiting.
+    """
+    choices = [0] * 90 + [60] * 10 
+    choice = random.choice(choices)
+
+    if choice ==  60:
+        time.sleep(5)
+    
+    return True
 
 
 def request(user_id):
     run_classifier()
     get_external_data()
+
     # new_request_count = increment_request_count(user_id)
     pass
